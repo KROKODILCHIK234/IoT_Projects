03ad8d241e739d2033d89fe25f539b5b57455050
KROKODILCHIK234 <159097510+KROKODILCHIK234@users.noreply.github.com>
2025-10-04 19:28:47 +0800

    Update LedCascade(TimersRegisters).cpp

--- LedCascade(TimersRegisters)/LedCascade(TimersRegisters).cpp
+++ LedCascade(TimersRegisters)/LedCascade(TimersRegisters).cpp
@@ -1,46 +1,63 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define LED_COUNT 5
+struct LedController {
+  const uint8_t pin;
+  const int reload_value;
+  int countdown;
+};
 
-const uint8_t led_portb_pins[LED_COUNT] = {PB0, PB1, PB2, PB4, PB5};
-const int blink_tick_rates[LED_COUNT] = {10, 20, 30, 40, 50};
+LedController led_array[] = {
+  {PB0, 10, 10},
+  {PB1, 20, 20},
+  {PB2, 30, 30},
+  {PB4, 40, 40},
+  {PB5, 50, 50}
+};
 
-volatile unsigned long master_tick_count = 0;
+const int number_of_leds = sizeof(led_array) / sizeof(led_array[0]);
 
-void setup() {
-  uint8_t portb_init_mask = 0;
-  for (int i = 0; i < LED_COUNT; i++) {
-    portb_init_mask |= (1 << led_portb_pins[i]);
+void configure_gpio() {
+  uint8_t pin_mask = 0;
+  for (int i = 0; i < number_of_leds; i++) {
+    pin_mask |= (1 << led_array[i].pin);
   }
+  DDRB |= pin_mask;
+  PORTB &= ~pin_mask;
+}
 
-  DDRB |= portb_init_mask;
-  PORTB &= ~portb_init_mask;
-
+void configure_timer_interrupts() {
   cli();
-
+  
   TCCR1A = 0;
   TCCR1B = 0;
+  
   TCCR1B |= (1 << WGM12);
   TCCR1B |= (1 << CS11) | (1 << CS10);
   
   OCR1A = 9999;
   
   TIMSK1 |= (1 << OCIE1A);
-
+  
   sei();
 }
 
-ISR(TIMER1_COMPA_vect) {
-  master_tick_count++;
+void setup() {
+  configure_gpio();
+  configure_timer_interrupts();
+}
 
-  for (int i = 0; i < LED_COUNT; i++) {
-    if ((master_tick_count % blink_tick_rates[i]) == 0) {
-      PORTB ^= (1 << led_portb_pins[i]);
+ISR(TIMER1_COMPA_vect) {
+  for (int i = 0; i < number_of_leds; i++) {
+    led_array[i].countdown--;
+
+    if (led_array[i].countdown == 0) {
+      PORTB ^= (1 << led_array[i].pin);
+      led_array[i].countdown = led_array[i].reload_value;
     }
   }
 }
 
 void loop() {
-  
+
 }
